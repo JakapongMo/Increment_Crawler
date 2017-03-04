be1db7f8b181de58a475f72323ec3da5aa99d960
Jakapong Klainongsuang <tengmo02@gmail.com>
2017-03-04 14:54:38 +0700

    can find url

---
+++ Direach.py
@@ -0,0 +1,102 @@
+import sys
+import glob
+import urllib.parse
+
+path = '/home/tengmo/workwithcrawler/2000file/*.arc'
+#path = '/home/tengmo/workwithcrawler/store/*.arc'
+
+files = glob.glob(path)
+
+####################-subdef-#######################
+def find_str(s, char):
+    index = 0
+
+    if char in s:
+        c = char[0]
+        for ch in s:
+            if ch == c:
+                if s[index:index+len(char)] == char:
+                    return True
+
+            index += 1
+
+    return False
+
+####################-maindef-#####################
+def File_URL(f):
+    URL = ''
+    Nb_slash = 0
+    before = ''
+    for line in f:
+        for char in line:
+            if char == '/':
+                Nb_slash +=1
+            if char  == ' ':
+                if before == '/':
+                    Nb_slash -=1
+                break
+            URL += char
+            before = char
+        break
+
+    URL = urllib.parse.unquote(URL)
+    yield URL
+    yield Nb_slash - 2
+    yield len(URL)- 7 - Nb_slash +1
+
+def File_binary_domain(URL):
+    Domain_name = ''
+    for char in URL[7:]:
+        if(char == '/'):
+            break
+        Domain_name += char
+    print(Domain_name)
+
+    list_string = ['.com', '.edu', '.gov', '.org', '.net', '.mil', '.co.th', '.in.th', '.ac.th']
+    list_domain = [0,0,0,0,0,0,0,0,0,0]
+    for x in range(0,9):
+        if(find_str(Domain_name, list_string[x])):
+            list_domain[x] += 1
+    count = 0
+    for x in range(0,9):
+        if(list_domain[x] == 0):
+            count +=1
+    if count == 9:
+        list_domain[9] +=1
+
+    print("binary_vector")
+    for x in range(0,10):
+        print(list_domain[x])
+
+
+#    com = 0
+#    edu = 0
+#    gov = 0
+#    org = 0
+#    net = 0
+#    mil = 0
+#    co_th = 0
+#    in_th = 0
+#    ac_th = 0
+#    other = 0
+
+
+cnt = 0
+for name in files:
+    try:
+        with open(name) as f:
+            #sys.stdout.write(f.read())
+            cnt +=1
+        #    if cnt == 3:
+        #        break
+            print(name ,' ',cnt)
+            URL, Nb_slash, URL_length = File_URL(f)
+            print(URL,'\n',Nb_slash,'\n',URL_length)
+            File_binary_domain(URL)
+        #    for line in f:
+        #        print (line)
+
+    except IOError as exc:
+        if exc.errno != errno.EISDIR:
+            print('Yo!!')
+            raise
--- test.py
+++ test.py
@@ -1,10 +1,18 @@
 import sys
+import os
 #print(sys.version+'\n')
 
-file_path = '/home/tengmo/workwithcrawler/testcrawl.arc'
 
-def find_date(path):
-    f = open(path)
+file_path = '/home/tengmo/Desktop/Project crawler/filtercrawl-20170101054747-00000.arc'
+#file_path = '/home/tengmo/workwithcrawler/testcrawl.arc'
+#file_path = '/home/tengmo/workwithcrawler/filter10000.arc'
+#file_path = '/home/tengmo/workwithcrawler/test.txt'
+sub_file_path = '/home/tengmo/workwithcrawler/sub_testcrawl.arc'
+
+
+
+def Find_date(path):
+    f = open(path, "r")
     date = ''
     for line in f:
         #print (line)
@@ -14,23 +22,43 @@ def find_date(path):
         break
     f.close()
 
-def find_URL(line):
+def Find_URL(line):
     URL = ''
     for nb in range(0,len(line)-41):
         URL += line[nb]
     print ("URL : ",URL)
 
+def Cal_Block(sub_f):
+    print ("haha")
+    sub_f = open(sub_file_path)
+    for line in sub_f:
+        print (line)
+
 #####################-main-############################
 
-date = find_date(file_path)
+date = Find_date(file_path)
 print (date+'\n')
 
-cnt = 1
-f = open(file_path)
+cnt = 0
+check = 1
+f = open(file_path, "r", errors='ignore')
+sub_f = open('/home/tengmo/workwithcrawler/store/sub_testcrawl[1].arc', 'w+')
 for line in f:
     if len(line) > 7:
         if str(line[0]+line[1]+line[2]+line[3]+line[4]+line[5]+line[6]) == 'http://':
-            print ("block",cnt)
-            find_URL(line)
             cnt += 1
+            print ("block",cnt)
+            #Find_URL(line)
     print (line)
+    if cnt == check:
+        sub_f.write(line)
+    ###เช็คว่ามันควรคำนวณรึยัง###
+    if cnt - check == 1:
+        sub_f.close
+        #Cal_Block(sub_f)
+        #if cnt == 2:
+        #    break
+        ############################os.remove(sub_file_path)
+        sub_f = open('/home/tengmo/workwithcrawler/store/sub_testcrawl[%d].arc'%cnt, 'w+')
+        sub_f.write(line)
+        check +=1
